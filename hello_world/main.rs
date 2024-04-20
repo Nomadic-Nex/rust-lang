e4e08ec8aedbd2518fc75857ca9b67f2e40bdc64
VVS Basanth Pedapati <160536337+Nomadic-Nex@users.noreply.github.com>
2024-04-20 20:09:09 +0530

    hello_world

---
+++ hello_world/main.rs
@@ -0,0 +1,6 @@
+//The lines define a function named 'main'.
+//this 'main' declares a funtion that has no parameters and returns nothing.
+fn main() {
+    println!("Hello, world!"); //This line prints text to the screen.
+}
+//If there were parameteres, they would go inside the parantheses ().
